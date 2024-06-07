780e5b544400e67b37aa4ca6f33c929c6d67bf29
mekha <mekhakishore3@gmail.com>
2024-06-07 12:11:58 +0530

    api for customers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+from rest_framework.viewsets import ModelViewSet
+
+from rest_framework.response import Response
+
+from rest_framework import authentication,permissions
+
+from api.serializers import CustomerSeializer
+
+from api.models import Customer
+
+
+class CustomerViewSetView(ModelViewSet):
+
+    serializer_class=CustomerSeializer
+
+    queryset=Customer.objects.all()
+
+    authentication_classes=[authentication.TokenAuthentication]
+
+    permission_classes=[permissions.IsAdminUser]
+
+    def perform_create(self, serializer):
+        serializer.save(technician=self.request.user)
+
+
 
